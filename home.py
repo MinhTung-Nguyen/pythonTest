336ef8f1a67a60c8398b06d39d72bc41e60f15f3
tungnguyen <minhtung.uit@gmail.com>
2025-06-01 16:18:55 +0700

    init

---
+++ home.py
@@ -0,0 +1,17 @@
+def menu():
+    print("Welcome to the Home Menu!")
+    print("1. View Profile")
+    print("2. Settings")
+    print("3. Logout")
+    
+    choice = input("Please select an option (1-3): ")
+    
+    if choice == '1':
+        view_profile()
+    elif choice == '2':
+        settings()
+    elif choice == '3':
+        logout()
+    else:
+        print("Invalid choice, please try again.")
+        menu()
